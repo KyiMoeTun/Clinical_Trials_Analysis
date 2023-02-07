6b1603004678ce4d75829b9e9ca6c894c6ea2bd6
KyiMoeTun <thisismyveryfirstemailaddress@gmail.com>
2023-02-06 23:50:31 -0600

    Third

--- Treatment_Comparison/trial_analysis/Analysis_of_clinical_trial.R
+++ Treatment_Comparison/trial_analysis/Analysis_of_clinical_trial.R
@@ -1,3 +1,7 @@
+#the following tutorial has been taken from the Clinical Trial Data Analysis Using SAS and R
+#Demonstration purposes only
+#t-test, Wilcoxon test, Welch test, F-tests are implemented in this r code
+
 
 
 #read the data from the above folder
